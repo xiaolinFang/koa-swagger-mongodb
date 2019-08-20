c0416b78ac11b1e8b1803d62c61cc3cb5f099925
Felix_f <893281878@qq.com>
2019-08-20 09:04:12 +0800

    -''

---
+++ src/routes/generateRoutes/audits.js
@@ -0,0 +1,208 @@
+import {
+  request,
+  summary,
+  body,
+  tags,
+  middlewares,
+  path,
+  description,
+  query
+} from '../../../dist';
+import dbClient from '../../middleware/db';
+// .toUpperCase()
+const tag = tags([
+  'audits'
+    .toLowerCase()
+    .replace('audits'.charAt(0), 'audits'.charAt(0).toUpperCase())
+]);
+
+const upDateJson = {
+  condition: {
+    type: 'object',
+    require: 'true',
+    description: 'Update the conditional json string'
+  },
+  json: {
+    type: 'object',
+    require: 'true',
+    description: 'Update the data json string'
+  }
+};
+const queryConditions = {
+  jsonStr: {
+    type: 'string',
+    description: 'a jsons data string or condition'
+  },
+  page: {
+    type: 'number',
+    description: 'The current page number "Not set to query all"'
+  },
+  pageSize: {
+    type: 'number',
+    description: 'Number of data bars per page "Not set to show all"'
+  },
+  filterFileds: {
+    type: 'string',
+    description:
+      '字段过滤条件 除_id 外，其他不同字段不能同时设置显示和隐藏，只能二选一'
+  }
+};
+
+const logTime = () => async (ctx, next) => {
+  console.time('start');
+  await next();
+  console.timeEnd('start');
+};
+export default class audits {
+  // 增
+  @request('POST', '/audits/add')
+  @summary('add audits')
+  @description('add a audits')
+  @tag
+  @middlewares([logTime()])
+  @body({})
+  static async add(ctx) {
+    const params = ctx.request.body;
+    if (!Object.keys(params).length) {
+      ctx.body = {
+        code: 400,
+        message: '缺少添加数据'
+      };
+      return;
+    }
+    const _checkHasDone = async () => {
+      const json = {
+        type: params.type
+      };
+      json['obj._id'] = params.obj._id;
+      console.log(json, '/json');
+
+      const hasdone = await dbClient.find('audits', json);
+      if (hasdone.count) {
+        return true;
+      }
+      return false;
+    };
+    if (!_checkHasDone()) {
+      const result = await dbClient.insert('audits', params);
+
+      ctx.body = {
+        code: result.code,
+        data: result.data.result
+      };
+    } else {
+      ctx.body = {
+        code: 500,
+        message: '已经提交申请无效，请勿重复申请'
+      };
+    }
+  }
+  // 删
+  @request('DELETE', '/audits/delete')
+  @summary('delete audits by condition')
+  @tag
+  @body({})
+  // @path({ id: { type: 'string', required: true } })
+  static async deleteMany(ctx) {
+    const params = ctx.request.body;
+    let paramsData = {};
+    if (params.jsonStr !== undefined) {
+      try {
+        paramsData =
+          typeof params.jsonStr === 'string'
+            ? JSON.parse(params.jsonStr)
+            : params.jsonStr;
+        if (paramsData._id) {
+          paramsData._id = dbClient.getObjectId(paramsData._id);
+        }
+        const result = await dbClient.remove('audits', paramsData);
+        ctx.body = result;
+      } catch (e) {
+        // console.log('Jsonstr is not a json string',e)
+        throw Error('Jsonstr is not a json string');
+      }
+    } else {
+      ctx.body = {
+        code: 500,
+        message: 'Jsonstr is undefined'
+      };
+    }
+  }
+  // 改
+  @request('Put', '/audits/update')
+  @summary('update audits')
+  @description('update a audits')
+  @tag
+  @middlewares([logTime()])
+  @body(upDateJson)
+  static async updateData(ctx, next) {
+    const params = ctx.request.body;
+    const condition = {};
+    const postData = {};
+    let result = {};
+    // if(params.condition !== undefined && params.jsonStr !== undefined){
+    //   try {
+    //     condition = typeof params.condition === 'string' ? JSON.parse(params.condition) : params.condition
+    //     postData = typeof params.jsonStr === 'string' ? JSON.parse(params.jsonStr) : params.jsonStr
+    //     result = await dbClient.update('audits',condition,postData)
+    //   } catch (e) {
+    //     console.log(e);
+    //     throw Error('Jsonstr is not a json string')
+    //   }
+    //   ctx.body = result
+    // }
+    if (params.json !== undefined && params.condition !== undefined) {
+      try {
+        delete params.json._id;
+        condition._id = dbClient.getObjectId(params.condition._id);
+        result = await dbClient.update('config', condition, params.json);
+      } catch (e) {
+        // console.log(e);
+        throw Error('jsonStr is not a json string ');
+      }
+    } else {
+      ctx.body = {
+        code: 500,
+        message: params.condition
+          ? 'jsonStr undefined'
+          : params.json
+            ? 'condition json string undefined'
+            : ' condition and jsonStr json string all undefined or {}'
+      };
+    }
+  }
+  // 查
+  @request('get', '/audits/find')
+  @summary('audits list / query by condition')
+  @query(queryConditions)
+  @tag
+  static async getAll(ctx) {
+    const params = ctx.request.query;
+    let filterConditions = {};
+    let paramsData = {};
+    if (params.jsonStr && params.jsonStr !== undefined) {
+      try {
+        paramsData = JSON.parse(params.jsonStr);
+      } catch (e) {
+        throw Error('Jsonstr is not a json string');
+      }
+    }
+    if (params.filterFileds) {
+      filterConditions = JSON.parse(params.filterFileds);
+    }
+    if (paramsData._id) {
+      paramsData._id = dbClient.getObjectId(paramsData._id);
+    }
+    const result =
+      params.page && params.pageSize
+        ? await dbClient.find(
+          'audits',
+          paramsData,
+          filterConditions,
+          params.page,
+          params.pageSize
+        )
+        : await dbClient.find('audits', paramsData, filterConditions);
+    ctx.body = result;
+  }
+}
